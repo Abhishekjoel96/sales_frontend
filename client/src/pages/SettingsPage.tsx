440fbb4177854e84dfd4864750d758ed37718f3a
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 01:32:12 +0530

    Update SettingsPage.tsx

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,13 +1,13 @@
-//src/pages/SettingsPage.tsx
-
-import React from 'react'
-import { FineTuneView } from '../components/FineTuneView'
+// src/pages/SettingsPage.tsx
+// Assuming this is where you'll put the FineTuneView
+import React from 'react';
+import { FineTuneView } from '../components/FineTuneView';
 import { useApp } from '../contexts/AppContext';
 
 const SettingsPage: React.FC = () => {
     const { theme } = useApp();
   return (
-    <FineTuneView theme={theme}/>
+     <FineTuneView theme={theme}/>
   )
 }
 

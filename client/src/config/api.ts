0e63f505f469817eaadd8a26bdf0998f67494094
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 01:18:58 +0530

    Update api.ts

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,13 +1,13 @@
 // src/config/api.ts
 import axios from 'axios';
+import config from '../../../backend/src/config/config';
 
 const api = axios.create({
     baseURL: 'https://sales-project-1.onrender.com/api', // Your Render backend URL
     headers: {
         'Content-Type': 'application/json',
-        'X-API-Key': import.meta.env.VITE_API_KEY,  //  Get API key from environment
+        'X-API-Key': config.apiKey,  // Use the API key from the backend config
     },
 });
-
 
 export default api;

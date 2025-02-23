17c82dbef8875925400de7f79d45befca642da6c
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 01:32:32 +0530

    Update ReportsPage.tsx

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,13 +1,15 @@
 // src/pages/ReportsPage.tsx
 import React from 'react';
-import { CallReportView } from '../components/CallReportView';
+// import { CallReportView } from '../components/CallReportView'; // Make sure this path is correct
 import { useApp } from '../contexts/AppContext';
+import { AICallingView } from '../components/AICallingView';
 
 
 const ReportsPage = () => {
-    const { theme } = useApp();
+    const { theme, leads } = useApp();
     return (
-        <CallReportView theme={theme} />
+        // <CallReportView theme={theme} />
+        <AICallingView theme={theme} leads={leads}/> // Assuming you'll display call reports within AICallingView
     );
 };
 

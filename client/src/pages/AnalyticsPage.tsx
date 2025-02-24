06a683dd16baf9915394279262b13c8ce471940c
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 11:39:12 +0530

    Update AnalyticsPage.tsx

--- client/src/pages/AnalyticsPage.tsx
+++ client/src/pages/AnalyticsPage.tsx
@@ -1,12 +1,13 @@
 // src/pages/AnalyticsPage.tsx
+
 import React from 'react';
 import { AnalyticsView } from '../components/AnalyticsView';
 import { useApp } from '../contexts/AppContext';
 
 const AnalyticsPage: React.FC = () => {
-  const { theme } = useApp();
+    const { theme } = useApp();
   return (
-     <AnalyticsView theme={theme} />
+     <AnalyticsView theme={theme}/>
   )
 }
 

c91e209bae2c6460aead5286f32853ac42deb53f
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 10:18:20 +0530

    Update ReportsPage.tsx

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,6 +1,6 @@
 // src/pages/ReportsPage.tsx
 import React from 'react';
-// import { CallReportView } from '../components/CallReportView'; // Make sure this path is correct
+import { CallReportView } from '../components/CallReportView'; // Make sure this path is correct
 import { useApp } from '../contexts/AppContext';
 import { AICallingView } from '../components/AICallingView';
 

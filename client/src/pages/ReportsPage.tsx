47b6595302cf3bf5b0daf21023c336ce48419371
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-24 16:44:49 +0530

    Update ReportsPage.tsx

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,14 +1,14 @@
 // src/pages/ReportsPage.tsx
-
 import React from 'react';
+//Corrected path
 import { CallReportView } from '../components/CallReportView'; // Make sure this path is correct
 import { useApp } from '../contexts/AppContext';
 
 
 const ReportsPage = () => {
-    const { theme } = useApp();
+    const { theme, leads, callLogs } = useApp(); // Get required data from context
     return (
-        <CallReportView theme={theme} />
+        <CallReportView theme={theme} callLogs={callLogs} leads={leads} />
 
     );
 };

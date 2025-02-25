bdfa5914859afc1272b21635d7781eab1e62c0b9
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-25 15:37:36 +0530

    Update callService.ts

--- client/src/services/callService.ts
+++ client/src/services/callService.ts
@@ -1,21 +1,17 @@
-//src/services/callService.ts
-
+// src/services/callService.ts
 import api from '../config/api';
 import { CallLog } from '../models/CallLog';
 
 export const makeCall = async (to: string, leadId: string, language: string): Promise<CallLog> => {
-  const response = await api.post('/calls/makeCall', { to, leadId, language });
-  return response.data;
+    const response = await api.post('/calls/makeCall', { to, leadId, language });
+    return response.data;
 };
 
 export const getAllCallLogs = async (): Promise<CallLog[]> => {
-  const response = await api.get('/calls/callLogs');
-  return response.data;
+    const response = await api.get('/calls/callLogs');
+    return response.data;
 }
-
 export const getCallLog = async(id: string): Promise<CallLog> => {
     const response = await api.get(`/calls/callLogs/${id}`);
     return response.data;
 }
-
-// No need for transcribeCall in the frontend.  It's triggered by a webhook.

831f2d6e32a69157ade047ba3043c3e0e04ff1b4
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-02-26 14:39:49 +0530

    Update api.ts

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL || 'https://sales-project-1.onrender.com/api', // Use VITE_ prefix
     headers: {
         'Content-Type': 'application/json',
-        'X-API-Key': import.meta.env.VITE_API_KEY,  // Use VITE_ prefix and your frontend API key
+        'X-API-Key': import.meta.env.VITE_API_KEY,  // Use VITE_ prefix and your frontend API key.
     },
 });
 
